6163a6971d19a5d313c95a68f338f1d04477ac73
Romain Pelisse <rpelisse@users.sourceforge.net>
2011-09-19 17:22:34 +0000

    add missing option to release's script usage()
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@7284 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/release.sh
+++ pmd/etc/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-    echo "$(basename ${0}) [-v version-number] [-d] [-s]"
+    echo "$(basename ${0}) [-v version-number] [-d] [-s] [-S]"
     echo ""
     echo "-v override the release's version number provided in pom.xml"
     echo "-d no docs generation"
