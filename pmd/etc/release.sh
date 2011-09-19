cd973949607521fb384656851f0e691c734546b4
Romain Pelisse <rpelisse@users.sourceforge.net>
2011-09-19 17:23:17 +0000

    add -p to mkdirs to prevent script from failing when directory does exist
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@7286 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/release.sh
+++ pmd/etc/release.sh
@@ -26,8 +26,8 @@ make_tree_structure() {
     local root_dir="${1}"
 
     mkdir -p "${root_dir}/etc"
-    mkdir "${root_dir}/bin"
-    mkdir "${root_dir}/lib"
+    mkdir -p "${root_dir}/bin"
+    mkdir -p "${root_dir}/lib"
 }
 
 while getopts v:dsh OPT; do
