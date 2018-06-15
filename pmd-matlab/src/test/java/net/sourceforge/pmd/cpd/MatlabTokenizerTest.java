5e39f95800f8e41de5bfa559486ee9704fb83b7f
Maikel Steneker <maikel.steneker@tiobe.com>
2018-06-15 12:05:48 +0200

    Added test cases for various characters in Matlab strings.

--- pmd-matlab/src/test/java/net/sourceforge/pmd/cpd/MatlabTokenizerTest.java
+++ pmd-matlab/src/test/java/net/sourceforge/pmd/cpd/MatlabTokenizerTest.java
@@ -53,4 +53,49 @@ public class MatlabTokenizerTest extends AbstractTokenizerTest {
         TokenEntry.getEOF();
         assertEquals(2, tokens.size()); // 2 tokens: "end" + EOF
     }
+
+    @Test
+    public void testLatexFormatting() throws IOException {
+        SourceCode sourceCode = new SourceCode(new SourceCode.StringCodeLoader("function MyFunction()" + PMD.EOL
+                + "  t = ProfileTranslation.t(1):step:ProfileTranslation.t(end) + time_extension;" + PMD.EOL
+                + "  x   = zeros(nrt, 1);" + PMD.EOL
+                + "  figure;" + PMD.EOL
+                + "  subplot(4, 2, 1)" + PMD.EOL
+                + "  plot(t, x);" + PMD.EOL
+                + "  xlabel('$\\phi$', 'Interpreter', 'latex');" + PMD.EOL
+                + "  ylabel('$x$', 'Interpreter', 'latex');" + PMD.EOL
+                + "  grid on;" + PMD.EOL
+                + "  hold on;" + PMD.EOL
+                + "  plot(wpt, wpx, 'ro');" + PMD.EOL
+                + "end"
+        ));
+        Tokens tokens = new Tokens();
+        tokenizer.tokenize(sourceCode, tokens); // should not result in parse error
+        TokenEntry.getEOF();
+        assertEquals(12, tokens.getTokens().get(tokens.getTokens().size() - 2).getBeginLine());
+    }
+
+    @Test
+    public void testUncPath() throws IOException {
+        SourceCode sourceCode = new SourceCode(new SourceCode.StringCodeLoader("paths = {..." + PMD.EOL
+                + "    '\\\\192.168.1.101\\My\\UNC_PATH\\20180615_123456\\'; ..." + PMD.EOL
+                + "    };" + PMD.EOL
+
+        ));
+        Tokens tokens = new Tokens();
+        tokenizer.tokenize(sourceCode, tokens); // should not result in parse error
+        TokenEntry.getEOF();
+        assertEquals(3, tokens.getTokens().get(tokens.getTokens().size() - 2).getBeginLine());
+    }
+
+    @Test
+    public void testBackslashInString() throws IOException {
+        SourceCode sourceCode = new SourceCode(new SourceCode.StringCodeLoader(
+                "path = strrep(copySource, '\\', '_');" + PMD.EOL + ";" + PMD.EOL
+        ));
+        Tokens tokens = new Tokens();
+        tokenizer.tokenize(sourceCode, tokens); // should not result in parse error
+        TokenEntry.getEOF();
+        assertEquals(2, tokens.getTokens().get(tokens.getTokens().size() - 2).getBeginLine());
+    }
 }
