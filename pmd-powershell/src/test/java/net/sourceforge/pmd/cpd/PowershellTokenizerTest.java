9f81f6a455e677d13b6f83dec5eac9d8ec7e5a96
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2024-03-11 15:03:55 +0100

    #33546: Add Powershell support to CPD
    Also fixed checkstyle violation in AntlrTokenizer.java

---
+++ pmd-powershell/src/test/java/net/sourceforge/pmd/cpd/PowershellTokenizerTest.java
@@ -0,0 +1,42 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.cpd;
+
+import java.util.Properties;
+
+import org.junit.Test;
+
+import net.sourceforge.pmd.cpd.test.CpdTextComparisonTest;
+
+public class PowershellTokenizerTest extends CpdTextComparisonTest {
+    public PowershellTokenizerTest() {
+        super(".ps1");
+    }
+
+    @Override
+    protected String getResourcePrefix() {
+        return "../lang/powershell/cpd/testdata";
+    }
+
+    @Override
+    public Tokenizer newTokenizer(Properties properties) {
+        return new PowershellTokenizer();
+    }
+
+    @Test
+    public void testInitMachine() {
+        doTest("Initialize-Machine-Signed");
+    }
+
+    @Test
+    public void testInstallVS() {
+        doTest("Install-VS");
+    }
+
+    @Test
+    public void testExpandableString() {
+        doTest("Expandable-String-Test");
+    }
+}
---
+++ pmd-powershell/src/test/resources/net/sourceforge/pmd/lang/powershell/cpd/testdata/Expandable-String-Test.ps1
@@ -0,0 +1,1 @@
+"This string contains a subexpression $($subExprVat + "with an inner expandable string $innerVar $(SubExpr $innerSubexprVar)") which has more variables"
